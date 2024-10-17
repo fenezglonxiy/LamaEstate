15ac3552fe565120f700b39ea033e658d63fd5ed
fenezglonxiy <thdathcmus@gmail.com>
2024-10-17 21:37:50 +0700

    #2 - Update elevation for PaperComponent

--- src/app/components/paper/paper.component.ts
+++ src/app/components/paper/paper.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-paper',
   standalone: true,
   imports: [],
   templateUrl: './paper.component.html',
-  styleUrl: './paper.component.scss'
+  styleUrl: './paper.component.scss',
 })
-export class PaperComponent {
+export class PaperComponent implements OnInit {
+  @Input()
+  elevation: 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12 | 16 | 24 = 0;
 
+  @HostBinding('class')
+  private _class = '';
+
+  ngOnInit(): void {
+    this._class = `elevation-${this.elevation}`;
+  }
 }
