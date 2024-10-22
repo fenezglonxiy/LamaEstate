7bda904f3cdad08414384ed70724d10f10d49398
fenezglonxiy <thdathcmus@gmail.com>
2024-10-22 16:34:18 +0700

    #2 - Add z-index for Overlay and PopoverAnchor, add background-color for Paper

--- src/app/components/popover/index.ts
+++ src/app/components/popover/index.ts
@@ -1,5 +1,4 @@
 export { PopoverContentComponent } from './popover-content/popover-content.component';
-export { PopoverContentDirective } from './popover-content/popover-content.directive';
 export { PopoverContentRegistratorComponent } from './popover-content-registrator/popover-content-registrator.component';
 export { PopoverTriggerComponent } from './popover-trigger/popover-trigger.component';
 export { PopoverTriggerDirective } from './popover-trigger/popover-trigger.directive';
