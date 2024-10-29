7ab59a7b5fd4f75cbb9801c14410a0f9acc6ec08
fenezglonxiy <thdathcmus@gmail.com>
2024-10-29 07:02:34 +0700

    #2 - Add EstateLocationMapComponent and change location of estate list pagination

--- src/app/modules/estate-list/estate-location-map/estate-location-map.component.ts
+++ src/app/modules/estate-list/estate-location-map/estate-location-map.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { circle, icon, latLng, marker, polygon, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-estate-list-estate-location-map',
   standalone: true,
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './estate-location-map.component.html',
   styleUrl: './estate-location-map.component.scss',
 })
-export class EstateLocationMapComponent {}
+export class EstateLocationMapComponent {
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      }),
+    ],
+    zoom: 12,
+    center: latLng(53.4084, -2.9916),
+  };
+
+  layer = marker(latLng(53.4084, -2.9916), {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png',
+      iconRetinaUrl: 'assets/marker-icon-2x.png',
+      shadowUrl: 'assets/marker-shadow.png',
+    }),
+  });
+
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+      'Open Cycle Map': tileLayer(
+        'https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+    },
+    overlays: {
+      'Big Circle': circle([46.95, -122], { radius: 5000 }),
+      'Big Square': polygon([
+        [46.8, -121.55],
+        [46.9, -121.55],
+        [46.9, -121.7],
+        [46.8, -121.7],
+      ]),
+    },
+  };
+}
